1d8df1bf5b606d2118e80df3517dbf0117e4e240
BogdanDraghia <bgd.draghia@gmail.com>
2026-01-02 11:40:55 +0100

    feat(project): create project page and update routing

--- app/routes.ts
+++ app/routes.ts
@@ -5,5 +5,5 @@ export default [
   route('/blog', 'routes/blog.tsx'),
   route('/blog/:slug', 'routes/blog.$slug.tsx'),
   route('/contact', 'routes/contact.tsx'),
-  route('/projects', 'routes/projects.tsx'),
+  route('/projects', 'routes/project.tsx'),
 ] satisfies RouteConfig;
---
+++ app/routes/project.tsx
@@ -0,0 +1,41 @@
+import Tag from '~/components/modules/Projects/Tag';
+import projectsRawData from '@content/projects/data.json';
+import ProjectList from '~/components/modules/Projects/ProjectList';
+import style from './project.module.css';
+
+export interface TagData {
+  name: string;
+  color: string;
+}
+
+interface ProjectLink {
+  label: string;
+  url: string;
+  target?: '_blank' | '_self';
+}
+
+export interface ProjectData {
+  title: string;
+  thumbnail: string;
+  content: string;
+  tags: string[];
+  links: ProjectLink[];
+}
+
+export interface ProjectsCollection {
+  tags: TagData[];
+  projects: ProjectData[];
+}
+
+const data = projectsRawData as ProjectsCollection;
+
+const Projects = () => {
+  const { projects, tags } = data;
+  return (
+    <div className={style.projectContainer}>
+      <h1>Projects</h1>
+      <ProjectList projects={projects} tags={tags} />
+    </div>
+  );
+};
+export default Projects;
